024d5be91367b77cd0bcac93cf3967ec8b083d86
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 19:21:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  start = 2
+  if num > 1
+    range = (start..num-1).to_a
+    range.none? do |num_to_test| 
+      num % num_to_test == 0
+    end
+  else
+    false
+  end
+end
+
+
+#note none of the num should return true for none? to return true. If none of the num return true, then they are not divisible by the num and the num is prime num 
